595e8a81e5dea91cf213245645bb399575a7c4ed
DanielIndividual <Daniel@Individual.com>
2025-07-02 21:27:57 +0800

    Try conflict hot-fix

--- src/main/java/com/atDaniel/git/GitTest.java
+++ src/main/java/com/atDaniel/git/GitTest.java
@@ -3,7 +3,7 @@ package com.atDaniel.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("hothothothot");
         System.out.println("Hello World3333");
         System.out.println("Hello World");
         System.out.println("Hello World");
